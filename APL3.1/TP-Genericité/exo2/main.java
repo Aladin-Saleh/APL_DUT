e1db73c2095e169249319c2746227e93ed721051
saleh <aladi@MSI.localdomain>
2020-11-20 17:54:03 +0100

    ok

--- APL3.1/TP-Genericité/exo2/main.java
+++ APL3.1/TP-Genericité/exo2/main.java
@@ -1,16 +1,32 @@
 import java.util.*;
 
-public class main 
+public class main implements Comparator
 {
 
 
+
+
+                            
+
 	public static void main(String[] args) {
 		
-		System.out.println(Arrays.toString(args));
-		Object[] copy = Arrays.copyOf(args, 5); 
+		//System.out.println(Arrays.toString(args));
+		Object[] copy = Arrays.copyOf(args, args.length); 
+		for(int i = 0; i<copy.length;++i)
+		System.out.print(copy[i] + " ");
 
-		 for (int i = 0; i < copy.length; i++)  
-           System.out.println(copy[i] + " "); 
+		System.out.println(" ");
+		
+		 Comparator<Object> comp = Comparator.naturalOrder();
+
+		Arrays.sort(copy,comp);
+		System.out.println("Apres le tri : ");
+		System.out.print(Arrays.toString(copy));
+
+
+
+	
+
     
 
 
