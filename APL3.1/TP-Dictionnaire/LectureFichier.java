76d37ecaa9605c1b170ff66d1c9fc0308f021348
Aladin <aladin8857@gmail.com>
2020-12-02 01:11:51 +0100

    ok

--- APL3.1/TP-Dictionnaire/LectureFichier.java
+++ APL3.1/TP-Dictionnaire/LectureFichier.java
@@ -2,6 +2,10 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.awt.*;
+
 
 
 public class LectureFichier {
@@ -10,38 +14,46 @@ public class LectureFichier {
 String[] arrOfStr;
 int[] rgbColor = new int[3];
 
-    public LectureFichier()
+private Map<String,Color> keyColor = new HashMap<>();
+    public LectureFichier(Map key)
     {
+      this.keyColor = key;
+
+      try {
+        BufferedReader lecture = new BufferedReader(
+                                 new FileReader("rgb.txt"));
         try {
-            BufferedReader lecture = new BufferedReader(
-                                     new FileReader("rgb.txt"));
-            try {
-                String ligne;
-                while ((ligne = lecture.readLine()) != null) 
-                {
-                  arrOfStr = new String[2];
-                  arrOfStr[1] = ligne.trim();
-                  for(int i = 0; i < 3;i++)
-                  {
-                      arrOfStr = arrOfStr[1].split(i==2?"	":" ",2);
-                      arrOfStr[1] = arrOfStr[1].trim();
-                      rgbColor[i] = Integer.parseInt(arrOfStr[0].trim());
-                  }
-                  System.out.println(arrOfStr[1]);
-                  
-                }
-            } catch(IOException e) {
-              System.err.println("Erreur de lecture dans rgb.txt !");
-            }
-            try {
-              lecture.close();
-            } catch(IOException e) {
-              System.err.println("Erreur de fermeture de rgb.txt !");
-            }
-          } catch(FileNotFoundException e) {
-            System.err.println("Erreur d'ouverture de rgb.txt !");
-          }
+            String ligne;
+            while ((ligne = lecture.readLine()) != null) 
+            {
+              arrOfStr = new String[2];
+              arrOfStr[1] = ligne.trim();
+              for(int i = 0; i < 3;i++)
+              {
+                  arrOfStr = arrOfStr[1].split(i==2?"	":" ",2);
+                  arrOfStr[1] = arrOfStr[1].trim();
+                  rgbColor[i] = Integer.parseInt(arrOfStr[0].trim());
+              }
+              //System.out.println(arrOfStr[1]);
+              keyColor.put(arrOfStr[1], new Color(rgbColor[0],rgbColor[1],rgbColor[2]));
+              
+              
+            }   
+
+        } catch(IOException e) {
+          System.err.println("Erreur de lecture dans rgb.txt !");
+        }
+        try {
+          lecture.close();
+        } catch(IOException e) {
+          System.err.println("Erreur de fermeture de rgb.txt !");
+        }
+      } catch(FileNotFoundException e) {
+        System.err.println("Erreur d'ouverture de rgb.txt !");
+      }
     }
+
+    
 
 
 }
--- APL3.1/TP-Dictionnaire/Screen.java
+++ APL3.1/TP-Dictionnaire/Screen.java
@@ -16,64 +16,24 @@ import java.io.IOException;
 
 public class Screen extends JFrame {
     
+    private Map<String,Color> keyColor = new HashMap<>();
+    private String[] arrOfStr;
+    private int[] rgbColor = new int[3];
+    private JList<String> listColor;
+    private Set<String> colorData;
+    GridLayout gestionnaire = new GridLayout(1,2);
+
 public Screen()
 {
     this.setSize(500,200);
     this.setLocation(500, 500);
     this.setTitle("Couleur");
-    LectureFichier test = new LectureFichier();
-    Map<String,Color> keyColor = new HashMap<>();
-    String[] arrOfStr;
-    int[] rgbColor = new int[3];
-    JList<String> listColor;
-   
-    Set<String> colorData;
-    
-    GridLayout gestionnaire = new GridLayout(1,2);
-    this.setLayout(gestionnaire);
-    
 
+    this.setLayout(gestionnaire);
     JPanel listC = new JPanel();
     JPanel panAfficheColor = new JPanel();
-    
-    
-
-
-   
-
-    
-        try {
-            BufferedReader lecture = new BufferedReader(
-                                     new FileReader("rgb.txt"));
-            try {
-                String ligne;
-                while ((ligne = lecture.readLine()) != null) 
-                {
-                  arrOfStr = new String[2];
-                  arrOfStr[1] = ligne.trim();
-                  for(int i = 0; i < 3;i++)
-                  {
-                      arrOfStr = arrOfStr[1].split(i==2?"	":" ",2);
-                      arrOfStr[1] = arrOfStr[1].trim();
-                      rgbColor[i] = Integer.parseInt(arrOfStr[0].trim());
-                  }
-                  //System.out.println(arrOfStr[1]);
-                  keyColor.put(arrOfStr[1], new Color(rgbColor[0],rgbColor[1],rgbColor[2]));
-                  
-                  
-                }   
-
-            } catch(IOException e) {
-              System.err.println("Erreur de lecture dans rgb.txt !");
-            }
-            try {
-              lecture.close();
-            } catch(IOException e) {
-              System.err.println("Erreur de fermeture de rgb.txt !");
-            }
-          } catch(FileNotFoundException e) {
-            System.err.println("Erreur d'ouverture de rgb.txt !");
-          }
+    LectureFichier read = new LectureFichier(keyColor);
+        
             colorData = keyColor.keySet();
             String[] colorString = colorData.toArray(new String[colorData.size()]);
             listColor = new JList<String>(colorString);
@@ -81,8 +41,6 @@ public Screen()
             listColor.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
             listColor.setLayoutOrientation(JList.VERTICAL);
             listColor.setVisibleRowCount(-1);
-            //JScrollPane listScroller = new JScrollPane(listColor);
-            //listColor.setPreferredSize(new Dimension(250, 500));
             listC.add(listColor);
            
 
@@ -90,20 +48,12 @@ public Screen()
 
             panAfficheColor.setBackground(Color.BLACK);
             
-            this.add(listC,BorderLayout.WEST);
-            this.add(panAfficheColor,BorderLayout.EAST);
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        this.add(listC,BorderLayout.WEST);
+        this.add(panAfficheColor,BorderLayout.EAST);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
         while(true)
             {
                 System.out.println(listColor.getSelectedValue());
